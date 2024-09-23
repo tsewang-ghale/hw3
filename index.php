7548b7668b9772d54057a2ab9287392a8b579575
tsewang-ghale <Tsewang.g@ou.edu>
2024-09-23 15:10:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-
+<?php
+$pageTitle = "Home";
+include "view_header.php";
+?>
+    <h1>Welcome to the Home Page</h1>
+    <ul>
+        <li><a href="post.php">Go to Post Page</a></li>
+        <li><a href="get.php">Go to Get Page</a></li>
+    </ul>
+<?php
+include "view_footer.php";
+?>
