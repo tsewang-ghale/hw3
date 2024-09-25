5e3e7d4655638a9ddd26b381339b6955bc512115
tsewang-ghale <Tsewang.g@ou.edu>
2024-09-25 14:35:54 -0500

    Create view-instructors.php

---
+++ view-instructors.php
@@ -0,0 +1,25 @@
+<h1> Instructors </h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th> ID </th>
+      <th> Name </th>
+      <th>Offices </th> 
+      </tr> 
+    </thead>
+    <tbody> 
+<?php
+while ($instructor= $instructors -> fetch_assoc()){
+?>
+  <tr>
+    <td><?php echo $instructor['instructor_id']; ?> </td>
+    <td><?php echo $instructor['instructor_name']; ?></td>
+    <td><?php echo $instructor['office_number']; ?></td> 
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
