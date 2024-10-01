772a23e0334ff779ffba5e2c1f2c0efe42223913
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-01 17:50:13 -0500

    Update sections.php

--- sections.php
+++ sections.php
@@ -1,10 +1,10 @@
 <?php
 require_once("util-db.php");
-require_once("model-instructors.php");
+require_once("model-sections.php");
   
 $pageTitle = "Sections";
 include "view-header.php";
-$sections = selectSection();
+$sections = selectSections();
 include "view-sections.php";
 include "view-footer.php";
 ?>
