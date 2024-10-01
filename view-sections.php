1eebc4c4cc496607be2b390c56aaea11b702d877
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-01 17:39:54 -0500

    Create view-sections.php

---
+++ view-sections.php
@@ -0,0 +1,31 @@
+<h1> Instructors </h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th> ID </th>
+      <th> Insturctor_ID </th>
+      <th>Course_ID </th> 
+      <th>Semester </th> 
+      <th> Room </th> 
+      <th> Day_Time </th> 
+      </tr> 
+    </thead>
+    <tbody> 
+<?php
+while ($section= $sections -> fetch_assoc()){
+?>
+  <tr>
+    <td><?php echo $section['section_id']; ?> </td>
+    <td><?php echo $section['instructor_id']; ?></td>
+    <td><?php echo $section['course_id']; ?></td> 
+    <td><?php echo $section['semester']; ?></td> 
+    <td><?php echo $section['room']; ?></td> 
+    <td><?php echo $section['day_time']; ?></td> 
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
