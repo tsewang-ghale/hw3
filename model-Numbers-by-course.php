efb8e4f2a599fb8e7d8a8769160932630a280d83
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-01 23:18:35 -0500

    Update and rename model-sections-by-course.php to model-Numbers-by-course.php

--- model-Numbers-by-course.php
+++ model-Numbers-by-course.php
@@ -1,9 +1,9 @@
 <?php
-function selectSectionsByCourse($cid) {
+function selectNumbersByCourse($nid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT c.course_id, course_number, course_description, semester, room, day_time FROM `course` c join section s on s.course_id= ?");
-        $stmt->bind_param("i", $cid);
+        $stmt->bind_param("i", $nid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
