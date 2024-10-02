f47388c9977df6b6aec661f9fb74ee55b72cc604
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-02 00:10:21 -0500

    Create instructors-with-courses.php

---
+++ instructors-with-courses.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-instructors-wtih-courses.php");
+  
+$pageTitle = "Instructors with Courses";
+include "view-header.php";
+$instructors = selectInstructors();
+include "view-instructors-with-courses.php";
+include "view-footer.php";
+?>
