bdfef65f51dd5fb2b2e068b28101f0117fe8b25f
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-01 23:35:59 -0500

    Update sections-by-course.php

--- sections-by-course.php
+++ sections-by-course.php
@@ -4,7 +4,7 @@ require_once("model-sections-by-course.php");
   
 $pageTitle = "Sections by Course";
 include "view-header.php";
-$section = selectSectionsByCourse($_POST['cid']);
+$sections = selectSectionsByCourse($_POST['cid']);
 include "view-sections-by-course.php";
 include "view-footer.php";
 ?>
