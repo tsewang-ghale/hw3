0eb0df5b9c508015bde64e71097a0f257ab1b2ca
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-02 13:47:07 -0500

    Update model-courses.php

--- model-courses.php
+++ model-courses.php
@@ -12,4 +12,42 @@ function selectCourses() {
         throw $e;
     }
 }
+function insertCourses($cNumbers, $cDesc) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `course` ( `course_number`, `course_description`) VALUES (?, ?)`");
+        $stmt->bind_param("ss", $cNumber, $cDesc)
+        $success = $stmt->execute(); 
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+
+        function updateCourses($cNumbers, $cDesc, $cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `course` set `course_number` = ?, `course_description`= ? where course_id= ?`");
+        $stmt->bind_param("ssi", $cNumber, $cDesc, $cid)
+        $success = $stmt->execute(); 
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+        
+        function deleteCourses($cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from course where course_id = ?");
+        $stmt->bind_param("i",$cid)
+        $success = $stmt->execute(); 
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
 ?>
