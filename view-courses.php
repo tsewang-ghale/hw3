67a8974a607df4bff2d6d2cc8ae0bfe329a8e84a
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-01 23:15:52 -0500

    Update view-courses.php

--- view-courses.php
+++ view-courses.php
@@ -6,6 +6,7 @@
       <th> ID </th>
       <th> Number </th>
       <th> Description </th> 
+      <th> </th>
       </tr> 
     </thead>
     <tbody> 
@@ -16,6 +17,12 @@ while ($course= $courses -> fetch_assoc()){
     <td><?php echo $course['course_id']; ?> </td>
     <td><?php echo $course['course_number']; ?></td>
     <td><?php echo $course['course_description']; ?></td> 
+    <td>
+      <form method= "post" action= "sections-by-course.php">
+        <input type= "hidden" name = "cid" value= "<?php echo $course['course_id']; ?>">
+        <button type="submit" class="btn btn-primary">Sections</button>
+      </form>
+    </td>
   </tr>
 <?php
 }
