5d0af586c9e33595f71382e08b13499ab633e998
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-07 13:27:14 -0500

    Update and rename instructors.php to customers.php

--- customers.php
+++ customers.php
@@ -2,9 +2,9 @@
 require_once("util-db.php");
 require_once("model-instructors.php");
   
-$pageTitle = "Instructors";
+$pageTitle = "Customers";
 include "view-header.php";
-$instructors = selectInstructors();
-include "view-instructors.php";
+$instructors = selectCustomers();
+include "view-customers.php";
 include "view-footer.php";
 ?>
