647ae33f4923dcfd90a2d8464947abc88f7aa833
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-07 14:37:47 -0500

    Update view-sales.php

--- view-sales.php
+++ view-sales.php
@@ -8,23 +8,22 @@
       <th> Sale Date </th> 
       <th> Tax </th>
       <th> Shipping </th>
-        
       </tr> 
     </thead>
     <tbody> 
 <?php
-while ($Sale= $sales -> fetch_assoc()){
+while ($sale= $sales -> fetch_assoc()){
 ?>
   <tr>
-    <td><?php echo $Sale['sale_id']; ?> </td>
-    <td><?php echo $Sale['cust_id']; ?></td>
-    <td><?php echo $Sale['sale_date']; ?></td> 
-    <td><?php echo $Sale['tax']; ?></td> 
-    <td><?php echo $Sale['shipping']; ?></td> 
+    <td><?php echo $sale['sale_id']; ?> </td>
+    <td><?php echo $sale['cust_id']; ?></td>
+    <td><?php echo $sale['sale_date']; ?></td> 
+    <td><?php echo $sale['tax']; ?></td> 
+    <td><?php echo $sale['shipping']; ?></td> 
   
     <td>
       <form method= "post" action= "saleitems-by-sale.php">
-        <input type= "hidden" name = "cid" value= "<?php echo $Sale['sale_id']; ?>">
+        <input type= "hidden" name = "cid" value= "<?php echo $sale['sale_id']; ?>">
         <button type="submit" class="btn btn-primary">Sale Items</button>
       </form>
     </td>
