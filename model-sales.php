7b24cdd0e199965a3ee1d61f46b2ab20c0227f03
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-07 14:33:46 -0500

    Update model-sales.php

--- model-sales.php
+++ model-sales.php
@@ -2,7 +2,7 @@
 function selectSales() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT sale_id, cust_id, sale_date, tax, shipping FROM `sale`");
+        $stmt = $conn->prepare("SELECT sale_id, cust_id, sale_date, tax, shipping FROM `Sale`");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
