f34e438a1ebbb847d5ac4d388e3ae6e45da95063
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-07 14:17:12 -0500

    Update and rename model-courses-by-instructor.php to model-sales-by-customer.php

--- model-sales-by-customer.php
+++ model-sales-by-customer.php
@@ -1,8 +1,8 @@
 <?php
-function selectCoursesByInstructor($iid) {
+function selectSalesByCustomer($iid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT c.course_id, course_number, course_description, semester, room, day_time FROM `course` c join section s on s.course_id= c.course_id where s.instructor_id = ?");
+        $stmt = $conn->prepare("SELECT s.sale_id, cust_id, sale_date, tax, shipping FROM `sale` s join saleitem si on s.sale_id= si.sale_id where s.customer_id = ?");
         $stmt->bind_param("i", $iid);
         $stmt->execute();
         $result = $stmt->get_result();
