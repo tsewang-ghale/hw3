977c01d8b8427d160771e2c6e5685505568b581f
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-09 13:11:06 -0500

    Create saleitems-by-sale.php

---
+++ saleitems-by-sale.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-saleitems-by-sale.php");
+
+$pageTitle = "SaleItems by Sale";
+include "view-header.php";
+$saleitems = selectSaleitemsBySale($_POST['sid']);
+include "view-saleitems-by-sale.php";
+include "view-footer.php";
+?>
