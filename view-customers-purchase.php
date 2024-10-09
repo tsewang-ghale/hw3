a2cf2f8264f25f1188020bff6155675e0da03a33
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-09 00:54:59 -0500

    Update view-customers-purchase.php

--- view-customers-purchase.php
+++ view-customers-purchase.php
@@ -14,26 +14,18 @@
     </thead>
     <tbody> 
 <?php
-// Check if $sales is a valid result set and has data
-if ($sales && $sales->num_rows > 0) {
-    // Loop through each sale record
     while ($sale = $sales->fetch_assoc()) {
 ?>
       <tr>
-        <td><?php echo htmlspecialchars($sale['sale_id']); ?> </td>
-        <td><?php echo htmlspecialchars($sale['cust_id']); ?></td>
-        <td><?php echo htmlspecialchars($sale['sale_date']); ?></td> 
-        <td><?php echo htmlspecialchars($sale['tax']); ?></td> 
-        <td><?php echo htmlspecialchars($sale['shipping']); ?></td> 
-        <td><?php echo htmlspecialchars($sale['quantity']); ?></td>
-        <td><?php echo htmlspecialchars($sale['saleprice']); ?></td>
+        <td><?php echo($sale['sale_id']); ?> </td>
+        <td><?php echo($sale['cust_id']); ?></td>
+        <td><?php echo($sale['sale_date']); ?></td> 
+        <td><?php echo($sale['tax']); ?></td> 
+        <td><?php echo($sale['shipping']); ?></td> 
+        <td><?php echo($sale['quantity']); ?></td>
+        <td><?php echo($sale['saleprice']); ?></td>
       </tr>
 <?php
-    }
-} else {
-    // If no sales are found
-    echo "<tr><td colspan='7'>No purchases found for this customer.</td></tr>";
-}
 ?>
     </tbody>
   </table>
