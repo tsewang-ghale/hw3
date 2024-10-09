3c703ea3522c3d50c1fa53d3dc60f01fc8399bb4
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-08 22:52:18 -0500

    Update Customers.php

--- Customers.php
+++ Customers.php
@@ -4,7 +4,7 @@ require_once("model-Customers.php");
   
 $pageTitle = "Customers";
 include "view-header.php";
-$instructors = selectCustomers();
+$Customers = selectCustomers();
 include "view-Customers.php";
 include "view-footer.php";
 ?>
