5ecac36c61eda5b71cb4a79b90744cf9df683238
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-08 22:53:54 -0500

    Update and rename Customers.php to customers.php

--- customers.php
+++ customers.php
@@ -4,7 +4,7 @@ require_once("model-Customers.php");
   
 $pageTitle = "Customers";
 include "view-header.php";
-$Customers = selectCustomers();
-include "view-Customers.php";
+$customers = selectCustomers();
+include "view-customers.php";
 include "view-footer.php";
 ?>
