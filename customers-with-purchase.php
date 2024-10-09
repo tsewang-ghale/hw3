b734e1e369621c5d8f908b4f2632ce36fe98b446
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-09 13:28:39 -0500

    Update customers-with-purchase.php

--- customers-with-purchase.php
+++ customers-with-purchase.php
@@ -1,1 +1,10 @@
-
+<?php
+require_once("util-db.php");
+require_once("model-customers-with-purchase.php");
+  
+$pageTitle = "Customers with Purchase";
+include "view-header.php";
+$customers = selectCustomers();
+include "view-customers-with-purchase.php";
+include "view-footer.php";
+?>
