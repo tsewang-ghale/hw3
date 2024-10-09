55334bed9c09980602380d1599ddd98affe7b4ce
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-09 13:12:02 -0500

    Create model-saleitems-by-sale.php

---
+++ model-saleitems-by-sale.php
@@ -0,0 +1,32 @@
+<?php
+function selectCustomersPurchase($custId) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("
+            SELECT 
+                s.sale_id, 
+                c.cust_id, 
+                c.cust_firstname,
+                c.cust_lastname,
+                p.product_name, 
+                s.sale_date, 
+                s.tax, 
+                s.shipping, 
+                si.quantity, 
+                si.saleprice 
+            FROM Sale s
+            JOIN Customer c ON s.cust_id = c.cust_id
+            JOIN SaleItem si ON s.sale_id = si.sale_id
+            JOIN Product p ON si.product_id = p.product_id
+            WHERE c.cust_id = ?");
+        $stmt->bind_param("i", $custId);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
