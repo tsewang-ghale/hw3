ea5456063415b0260c84974fcf62b779e9b718dc
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-08 23:24:57 -0500

    Create model-customers-purchase.php

---
+++ model-customers-purchase.php
@@ -0,0 +1,15 @@
+<?php
+function selectCustomers() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT cust_id, cust_firstname, cust_lastname, cust_address, cust_phone, cust_email FROM `Customer`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
