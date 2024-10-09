09b0da58068455b03a77a410c399935ead7d3eeb
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-08 23:25:29 -0500

    Update and rename customers-purchases.php to customers-purchase.php

--- customers-purchase.php
+++ customers-purchase.php
@@ -1,10 +1,10 @@
 <?php
 require_once("util-db.php");
-require_once("model-customers-purchases.php");
+require_once("model-customers-purchase.php");
   
 $pageTitle = "Customers Purchases";
 include "view-header.php";
 $customers = selectCustomersPurchase($_GET ['id']);
-include "view-customers-purchases.php";
+include "view-customers-purchase.php";
 include "view-footer.php";
 ?>
