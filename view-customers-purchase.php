489245d52c5690ed1053354248b717fa5df9f6a1
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-09 01:13:08 -0500

    Update view-customers-purchase.php

--- view-customers-purchase.php
+++ view-customers-purchase.php
@@ -4,7 +4,8 @@
     <thead>
       <tr>
         <th>Sale ID</th>
-        <th>Customer Name</th>
+        <th>Customer First Name</th>
+        th>Customer Last Name</th>
         <th>Product Name</th>
         <th>Sale Date</th> 
         <th>Tax</th>
@@ -15,13 +16,13 @@
     </thead>
     <tbody> 
 <?php
-// Check if there are any sales to display
-if ($sales && $sales->num_rows > 0) {
-    while ($sale = $sales->fetch_assoc()) {
+
+while ($sale = $sales->fetch_assoc()) {
 ?>
       <tr>
         <td><?php echo($sale['sale_id']); ?></td>
-        <td><?php echo($sale['cust_name']); ?></td>
+        <td><?php echo($sale['cust_firstname']); ?></td>
+        <td><?php echo($sale['cust_lastname']); ?></td>
         <td><?php echo($sale['product_name']); ?></td>
         <td><?php echo($sale['sale_date']); ?></td> 
         <td><?php echo($sale['tax']); ?></td> 
@@ -31,9 +32,6 @@ if ($sales && $sales->num_rows > 0) {
       </tr>
 <?php
     }
-} else {
-    echo "<tr><td colspan='8'>No purchases found for this customer.</td></tr>";
-}
 ?>
     </tbody>
   </table>
