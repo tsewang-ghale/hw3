d5e30d60436fbd27666343afd43ec274f55d65d8
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-09 00:33:41 -0500

    Update customers-purchase.php

--- customers-purchase.php
+++ customers-purchase.php
@@ -4,7 +4,7 @@ require_once("model-customers-purchase.php");
   
 $pageTitle = "Customers Purchases";
 include "view-header.php";
-$sales = selectCustomersPurchase($_GET['id']);
+$sale = selectCustomersPurchase($_GET['id']);
 include "view-customers-purchase.php";
 include "view-footer.php";
 ?>
