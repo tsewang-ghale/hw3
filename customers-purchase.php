23e535fa6b959dc62c32fa0c0ab02dd02881a7ef
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-09 00:57:21 -0500

    Update customers-purchase.php

--- customers-purchase.php
+++ customers-purchase.php
@@ -4,8 +4,15 @@ require_once("model-customers-purchase.php");
 
 $pageTitle = "Customers Purchases";
 include "view-header.php";
-$sales = selectCustomersPurchase($_GET['id']);
-include "view-customers-purchase.php";
+
+// Check if customer ID is provided
+if (isset($_GET['id'])) {
+    $sales = selectCustomersPurchase($_GET['id']);
+    include "view-customers-purchase.php";
+} else {
+    echo "<p>Error: No customer ID provided.</p>";
+}
+
 include "view-footer.php";
 ?>
 
