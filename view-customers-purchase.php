a8a8a72e64a1756ef5f6a6ecc8fb664532951959
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-09 00:59:23 -0500

    Update view-customers-purchase.php

--- view-customers-purchase.php
+++ view-customers-purchase.php
@@ -1,24 +1,28 @@
-<h1> Customers Purchases </h1>
+<h1>Customers Purchases</h1>
 <div class="table-responsive">
   <table class="table">
     <thead>
       <tr>
-        <th> Sale ID </th>
-        <th> Customer ID </th>
-        <th> Sale Date </th> 
-        <th> Tax </th>
-        <th> Shipping </th>
-        <th> Quantity </th>
-        <th> Sale Price </th>
+        <th>Sale ID</th>
+        <th>Customer Name</th>
+        <th>Product Name</th>
+        <th>Sale Date</th> 
+        <th>Tax</th>
+        <th>Shipping</th>
+        <th>Quantity</th>
+        <th>Sale Price</th>
       </tr> 
     </thead>
     <tbody> 
 <?php
+// Check if there are any sales to display
+if ($sales && $sales->num_rows > 0) {
     while ($sale = $sales->fetch_assoc()) {
 ?>
       <tr>
-        <td><?php echo($sale['sale_id']); ?> </td>
-        <td><?php echo($sale['cust_id']); ?></td>
+        <td><?php echo($sale['sale_id']); ?></td>
+        <td><?php echo($sale['cust_name']); ?></td>
+        <td><?php echo($sale['product_name']); ?></td>
         <td><?php echo($sale['sale_date']); ?></td> 
         <td><?php echo($sale['tax']); ?></td> 
         <td><?php echo($sale['shipping']); ?></td> 
@@ -26,7 +30,12 @@
         <td><?php echo($sale['saleprice']); ?></td>
       </tr>
 <?php
+    }
+} else {
+    echo "<tr><td colspan='8'>No purchases found for this customer.</td></tr>";
+}
 ?>
     </tbody>
   </table>
 </div>
+
