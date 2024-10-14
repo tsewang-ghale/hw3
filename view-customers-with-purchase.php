bcf3a3f277d682a6cdf11c98435536b590a66349
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-14 15:45:33 -0500

    Update view-customers-with-purchase.php

--- view-customers-with-purchase.php
+++ view-customers-with-purchase.php
@@ -5,7 +5,7 @@ while ($customer= $customers -> fetch_assoc()){
 ?>
   <div class="card">
     <div class="card-body">
-      <h5 class="card-title"><?php echo $customer['cust_firstname']; ?>,<?php echo $customer['cust_lastname']; ?> </h5>
+      <h5 class="card-title"><?php echo $customer['cust_firstname']; ?>;<?php echo $customer['cust_lastname']; ?> </h5>
       <p class="card-text">
      <ul class="list-group">
   <?php
@@ -18,7 +18,7 @@ while ($customer= $customers -> fetch_assoc()){
   ?>
       </ul>
       </p>
-      <p class="card-text"><small class="text-body-secondary"> Phonenumber: <?php echo $customer['cust_phone']; ?>, Email:<?php echo $customer['cust_email']; ?> </small></p>
+      <p class="card-text"><small class="text-body-secondary"> Phonenumber: <?php echo $customer['cust_phone']; ?>; Email:<?php echo $customer['cust_email']; ?> </small></p>
     </div>
   </div>
 <?php
