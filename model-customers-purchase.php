64cb1198549995bb14d14c17e0bd2f88e4568985
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-16 13:37:31 -0500

    Update model-customers-purchase.php

--- model-customers-purchase.php
+++ model-customers-purchase.php
@@ -4,19 +4,14 @@ function selectCustomersPurchase($sale_id) {
         $conn = get_db_connection();
         $stmt = $conn->prepare("
             SELECT  
-                s.sale_id,
-                p.product_name, 
+                s.sale_id, 
                 s.sale_date, 
                 s.tax, 
-                s.shipping, 
-                si.quantity, 
-                si.saleprice 
+                s.shipping
             FROM Sale s
             JOIN Customer c ON s.cust_id = c.cust_id
-            JOIN SaleItem si ON s.sale_id = si.sale_id
-            JOIN Product p ON si.product_id = p.product_id
             WHERE c.cust_id = ?");
-        $stmt->bind_param("i", $sale_id);
+        $stmt->bind_param("i", $Sale_id);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
