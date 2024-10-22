dca0100f72ac452f0df2c182b5b188d284a10e3b
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-22 15:25:48 -0500

    Update view-sales.php

--- view-sales.php
+++ view-sales.php
@@ -17,6 +17,9 @@ include "view-sales-newform.php";
       <th> Sale Date </th> 
       <th> Tax </th>
       <th> Shipping </th>
+        <th> </th>
+        <th> </th>
+        <th> </th>
       </tr> 
     </thead>
     <tbody> 
@@ -29,8 +32,21 @@ while ($sale= $sales -> fetch_assoc()){
     <td><?php echo $sale['sale_date']; ?></td> 
     <td><?php echo $sale['tax']; ?></td> 
     <td><?php echo $sale['shipping']; ?></td> 
-  
+    <td> 
+    </td>
+    <td> 
+    </td>
     <td>
+      <form method= "post" action= "">
+        <input type= "hidden" name = "cid" value= "<?php echo $sale['sale_id']; ?>">
+        <input type= "hidden" name= "actionType" value = "Delete">
+        <button type="submit" class="btn btn-primary">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+          </svg>
+        </button>
+      </form>
       <form method= "post" action= "saleitems-by-sale.php">
         <input type= "hidden" name = "cid" value= "<?php echo $sale['sale_id']; ?>">
         <button type="submit" class="btn btn-primary">Sale Items</button>
