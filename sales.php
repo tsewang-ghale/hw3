df2009ddd6e8d66157d529b85bc0947af9ffdaa9
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-22 15:03:30 -0500

    Update sales.php

--- sales.php
+++ sales.php
@@ -8,7 +8,7 @@ include "view-header.php";
 if (isset($_POST ['actionType'])){
   switch ($_POST ['actionType']) {
     case "Add": 
-      insertSale($_POST[Cust_id'],  $_POST ['Sale_date'], $_POST ['Tax'], $_POST['Shipping']); 
+      insertSale($_POST['cid'],  $_POST ['saledate'], $_POST ['tax'], $_POST['shipping']); 
       break; 
   }
 }
