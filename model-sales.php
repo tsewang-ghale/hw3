3d0c6d34e39d53f9f5a9ccdbc2575c1ba5bcc475
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-22 13:47:16 -0500

    Update model-sales.php

--- model-sales.php
+++ model-sales.php
@@ -12,4 +12,43 @@ function selectSales() {
         throw $e;
     }
 }
+function InsertSale($cid, $saledate, $tax, $shipping) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `Sale` (`cust_id`, `sale_date`, `tax`, `shipping`) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("isii",$cid, $saledate, $tax, $shipping);
+        $success= $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function UpdateSale($sid, $cid, $saledate, $tax, $shipping) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `Sale` set `cust_id` = ?, `sale_date` =? , `tax`=? , `shipping`= ? where Sale_id = ?");
+        $stmt->bind_param("isii",$cid, $saledate, $tax, $shipping);
+        $success= $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deleteSale($sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from`Sale` where Sale_id=?");
+        $stmt->bind_param("i", $sid);
+        $success= $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
