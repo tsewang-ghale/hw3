12f3c6e1be61cd9aa5fb2b2b4fb7e5369f7d9e27
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-22 15:38:55 -0500

    Update sales.php

--- sales.php
+++ sales.php
@@ -16,7 +16,7 @@ if (isset($_POST ['actionType'])){
       break; 
     case "Delete":
       if (deleteSale($_POST['sid'])) {
-        echo '<div class="alert alert-success" role="alert"> Sale deleted.</div>'; 
+        echo '<div class="alert alert-success" role="alert"> Sale deleted.</div>"'; 
       } else {
         echo '<div class="alert alert-danger" role="alert"> Error.</div>';
       }
