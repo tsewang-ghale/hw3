4c2b9246dec3a2660043f415f3e9da9edb7c5fa9
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-22 15:01:04 -0500

    Update sales.php

--- sales.php
+++ sales.php
@@ -4,6 +4,15 @@ require_once("model-sales.php");
   
 $pageTitle = "Sales";
 include "view-header.php";
+
+if (isset($_POST ['actionType'])){
+  switch ($_POST ['actionType']) {
+    case "Add": 
+      insertSale($_POST['cust_id'],  $_POST ['saledate'], $_POST ['tax'], $_POST['shipping']); 
+      break; 
+  }
+}
+
 $sales = selectSales();
 include "view-sales.php";
 include "view-footer.php";
