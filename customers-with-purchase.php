7398332216fdee76caeb24eeef760668695b43cd
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-24 15:13:41 -0500

    Update customers-with-purchase.php

--- customers-with-purchase.php
+++ customers-with-purchase.php
@@ -7,11 +7,11 @@ include "view-header.php";
 if (isset($_POST ['actionType'])){
   switch ($_POST ['actionType']) {
     case "Add": 
-      if (insertSale($_POST['Cust_id'],$_POST['cust_firstname'], $_POST['cust_lastname'],$_POST ['product_name'], $_POST ['sale_date'], $_POST ['tax'], $_POST['shipping'], $_POST ['quantity'], $_POST ['saleprice'])) {
-        echo '<div class="alert alert-success" role="alert"> Sale added.</div>'; 
-      } else {
-        echo '<div class="alert alert-danger" role="alert"> Error.</div>';
-      }
+     if (insertSale($_POST['Cust_id'], $_POST['cust_firstname'], $_POST['cust_lastname'], $_POST['product_name'], $_POST['sale_date'], $_POST['tax'], $_POST['shipping'], $_POST['quantity'], $_POST['saleprice'])) {
+            echo '<div class="alert alert-success" role="alert"> Sale added.</div>'; 
+        } else {
+            echo '<div class="alert alert-danger" role="alert"> Error.</div>';
+        }
       break; 
     case "Edit": 
       if (UpdateSale($_POST['sid'], $_POST['custId'],$_POST['cust_firstname'], $_POST['cust_lastname'], $_POST['product_name'], $_POST['tax'], $_POST['shipping'], $_POST['quantity'], $_POST['saleprice'])) {
