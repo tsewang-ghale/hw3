ab1d6f5c874a255da3821ef1735a07e205e7a993
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-24 14:11:52 -0500

    Update model-customers-with-purchase.php

--- model-customers-with-purchase.php
+++ model-customers-with-purchase.php
@@ -42,31 +42,40 @@ function selectCustomersPurchase($custId) {
         throw $e;
     }
 }
-function insertCustomersPurchase($custId) {
+function insertCustomersPurchase($custId, $cfirstname, $clastname, $pname, $saledate, $tax, $shipping, $quantity, $saleprice ) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("
-            SELECT 
-                s.sale_id, 
-                c.cust_id, 
-                c.cust_firstname,
-                c.cust_lastname,
-                p.product_name, 
-                s.sale_date, 
-                s.tax, 
-                s.shipping, 
-                si.quantity, 
-                si.saleprice 
-            FROM Sale s
-            JOIN Customer c ON s.cust_id = c.cust_id
-            JOIN SaleItem si ON s.sale_id = si.sale_id
-            JOIN Product p ON si.product_id = p.product_id
-            WHERE c.cust_id = ?");
-        $stmt->bind_param("i", $custId);
-        $stmt->execute();
-        $result = $stmt->get_result();
+        $stmt = $conn->prepare("insert into 'Sale' ('cust_id', 'cust_firstname', 'cust_lastname', 'product_name', 'sale_date', 'tax', 'shipping', 'quantity', 'saleprice' )");
+        $stmt->bind_param("i", $custId, $cfirstname, $clastname,$clastname, $pname, $saledate, $tax, $shipping, $quantity, $saleprice );
+        $success = $stmt->execute();
         $conn->close();
-        return $result;
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function updateCustomersPurchase($Sale_id) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update into 'Sale' ('cust_id', 'cust_firstname', 'cust_lastname', 'product_name', 'sale_date', 'tax', 'shipping', 'quantity', 'saleprice' )");
+        $stmt->bind_param("i", $custId, $cfirstname, $clastname,$clastname, $pname, $saledate, $tax, $shipping, $quantity, $saleprice );
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function insertCustomersPurchase($custId, $cfirstname, $clastname, $pname, $saledate, $tax, $shipping, $quantity, $saleprice ) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("insert into 'Sale' ('cust_id', 'cust_firstname', 'cust_lastname', 'product_name', 'sale_date', 'tax', 'shipping', 'quantity', 'saleprice' )");
+        $stmt->bind_param("i", $custId, $cfirstname, $clastname,$clastname, $pname, $saledate, $tax, $shipping, $quantity, $saleprice );
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
     } catch (Exception $e) {
         $conn->close();
         throw $e;
