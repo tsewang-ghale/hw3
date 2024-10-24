06e06dc3072c65d0a18839d3666106cf298432ce
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-24 15:17:24 -0500

    Update customers-with-purchase.php

--- customers-with-purchase.php
+++ customers-with-purchase.php
@@ -7,21 +7,21 @@ include "view-header.php";
 if (isset($_POST ['actionType'])){
   switch ($_POST ['actionType']) {
     case "Add": 
-     if (insertSale($_POST['Cust_id'], $_POST['cust_firstname'], $_POST['cust_lastname'], $_POST['product_name'], $_POST['sale_date'], $_POST['tax'], $_POST['shipping'], $_POST['quantity'], $_POST['saleprice'])) {
+     if (insertCustomersPurchase($_POST['Cust_id'], $_POST['cust_firstname'], $_POST['cust_lastname'], $_POST['product_name'], $_POST['sale_date'], $_POST['tax'], $_POST['shipping'], $_POST['quantity'], $_POST['saleprice'])) {
             echo '<div class="alert alert-success" role="alert"> Sale added.</div>'; 
         } else {
             echo '<div class="alert alert-danger" role="alert"> Error.</div>';
         }
       break; 
     case "Edit": 
-      if (UpdateSale($_POST['sid'], $_POST['custId'],$_POST['cust_firstname'], $_POST['cust_lastname'], $_POST['product_name'], $_POST['tax'], $_POST['shipping'], $_POST['quantity'], $_POST['saleprice'])) {
+      if (UpdateCustomersPurchase($_POST['sid'], $_POST['custId'],$_POST['cust_firstname'], $_POST['cust_lastname'], $_POST['product_name'], $_POST['tax'], $_POST['shipping'], $_POST['quantity'], $_POST['saleprice'])) {
             echo '<div class="alert alert-success" role="alert"> Sale edited.</div>';
         } else {
             echo '<div class="alert alert-danger" role="alert"> Error.</div>';
         }
       break; 
     case "Delete":
-      if (deleteSale($_POST['sid'])) {
+      if (deleteCustomersPurchase($_POST['sid'])) {
         echo '<div class="alert alert-success" role="alert"> Sale deleted.</div>'; 
       } else {
         echo '<div class="alert alert-danger" role="alert"> Error.</div>';
@@ -30,7 +30,7 @@ if (isset($_POST ['actionType'])){
   }
 }
 
-$sales = selectSales();
+$customers = selectCustomers();
 include "view-customers-with-purchase.php";
 include "view-footer.php";
 ?>
