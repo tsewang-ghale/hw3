b6e3a2d92e77c1ed694de30252bb6693fc2c1e64
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-27 18:08:49 -0500

    Update saleitems-by-sale.php

--- saleitems-by-sale.php
+++ saleitems-by-sale.php
@@ -4,7 +4,7 @@ require_once("model-saleitems-by-sale.php");
 
 $pageTitle = "SaleItems by Sale";
 include "view-header.php";
-$saleitems = selectSaleitemsBySale($_POST['cid']);
+$saleitems = selectSaleitemsBySale($_POST['lid']);
 include "view-saleitems-by-sale.php";
 include "view-footer.php";
 ?>
