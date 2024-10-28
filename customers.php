6648f8553a1f6dfd28fc1db58e7300bbd3c86cab
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-28 13:20:18 -0500

    Update customers.php

--- customers.php
+++ customers.php
@@ -7,14 +7,14 @@ include "view-header.php";
 if (isset($_POST ['actionType'])){
   switch ($_POST ['actionType']) {
     case "Add": 
-      if (insertCustomer($_POST['first_name'],$_POST ['last_name'], $_POST ['Address'], $_POST['Phone'],$_POST['Email'])) {
+      if (insertCustomer($_POST['first_name'],$_POST ['last_name'], $_POST ['address'], $_POST['phone'],$_POST['email'])) {
         echo '<div class="alert alert-success" role="alert"> Customer added.</div>'; 
       } else {
         echo '<div class="alert alert-danger" role="alert"> Error.</div>';
       }
       break; 
     case "Edit": 
-      if (UpdateCustomer($_POST['cid'], $_POST['first_name'], $_POST ['last_name'], $_POST ['Address'], $_POST['Phone'],$_POST['Email'])) {
+      if (UpdateCustomer($_POST['cid'], $_POST['first_name'], $_POST ['last_name'], $_POST ['address'], $_POST['phone'],$_POST['email'])) {
             echo '<div class="alert alert-success" role="alert"> Customer edited.</div>';
         } else {
             echo '<div class="alert alert-danger" role="alert"> Error.</div>';
