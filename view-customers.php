5a913c635546a6b0de4d5c595b97ec55bef937a7
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-28 13:12:58 -0500

    Update view-customers.php

--- view-customers.php
+++ view-customers.php
@@ -1,4 +1,13 @@
+<div class = "row"> 
+  <div class = "col">
 <h1> Customers </h1>
+  </div>
+  <div class = "col-auto">
+<?php
+include "view-customers-newform.php"; 
+?>  
+  </div>
+</div>
 <div class="table-responsive">
   <table class="table">
     <thead>
