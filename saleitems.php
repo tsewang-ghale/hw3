a0fcbe4a2d0f1b6fa5e1314b08718155af9e3d64
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-28 14:58:31 -0500

    Update saleitems.php

--- saleitems.php
+++ saleitems.php
@@ -1,20 +1,20 @@
 <?php
 require_once("util-db.php");
 require_once("model-saleitems.php");
-  
+ 
 $pageTitle = "Sale Items";
 include "view-header.php";
 if (isset($_POST ['actionType'])){
   switch ($_POST ['actionType']) {
     case "Add": 
-      if (insertSaleItem($_POST['Cust_id'],$_POST ['Sale_date'], $_POST ['Tax'], $_POST['Shipping'])) {
+      if (insertSaleItem($_POST ['product_id'], $_POST ['sale_id'], $_POST['quantity'],$_POST['saleprice'] )) {
         echo '<div class="alert alert-success" role="alert"> Sale added.</div>'; 
       } else {
         echo '<div class="alert alert-danger" role="alert"> Error.</div>';
       }
       break; 
     case "Edit": 
-      if (UpdateSaleItem($_POST['sid'], $_POST['Sale_date'], $_POST['Tax'], $_POST['Shipping'])) {
+      if (UpdateSaleItem($_POST['saleitem_id'], $_POST['sale_id'], $_POST['quantity'], $_POST['saleprice'])) {
             echo '<div class="alert alert-success" role="alert"> Sale edited.</div>';
         } else {
             echo '<div class="alert alert-danger" role="alert"> Error.</div>';
@@ -22,8 +22,8 @@ if (isset($_POST ['actionType'])){
 
       break; 
     case "Delete":
-      if (deleteSaleItem($_POST['sale_id'])) {
-        echo '<div class="alert alert-success" role="alert"> Sale deleted.</div>'; 
+      if (deleteSaleItem($_POST['saleitem_id'])) {
+        echo '<div class="alert alert-success" role="alert"> SaleItem deleted.</div>'; 
       } else {
         echo '<div class="alert alert-danger" role="alert"> Error.</div>';
       }
