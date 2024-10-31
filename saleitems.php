f8a9c7ede5f61b2966e23f27323e011fc2b0a602
tsewang-ghale <Tsewang.g@ou.edu>
2024-10-31 13:55:17 -0500

    Update saleitems.php

--- saleitems.php
+++ saleitems.php
@@ -1,9 +1,35 @@
 <?php
 require_once("util-db.php");
 require_once("model-saleitems.php");
-  
+ 
 $pageTitle = "Sale Items";
 include "view-header.php";
+if (isset($_POST ['actionType'])){
+  switch ($_POST ['actionType']) {
+    case "Add": 
+      if (insertSaleItem($_POST ['product_id'], $_POST ['sale_id'], $_POST['quantity'],$_POST['saleprice'] )) {
+        echo '<div class="alert alert-success" role="alert"> SaleItem added.</div>'; 
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error.</div>';
+      }
+      break; 
+    case "Edit": 
+      if (UpdateSaleItem($_POST['product_id'], $_POST['sale_id'], $_POST['quantity'], $_POST['saleprice'])) {
+            echo '<div class="alert alert-success" role="alert"> SalItem edited.</div>';
+        } else {
+            echo '<div class="alert alert-danger" role="alert"> Error.</div>';
+        }
+
+      break; 
+    case "Delete":
+      if (deleteSaleItem($_POST['saleitem_id'])) {
+        echo '<div class="alert alert-success" role="alert"> SaleItem deleted.</div>'; 
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error.</div>';
+      }
+      break;
+  }
+}
 $saleitems = selectSaleItems();
 include "view-saleitems.php";
 include "view-footer.php";
