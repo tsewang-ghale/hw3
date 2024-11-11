f562a6388a88115d6c62c7ea4411349a34ed7343
tsewang-ghale <Tsewang.g@ou.edu>
2024-11-11 00:24:29 -0600

    Update saleitems.php

--- saleitems.php
+++ saleitems.php
@@ -7,14 +7,14 @@ include "view-header.php";
 if (isset($_POST ['actionType'])){
   switch ($_POST ['actionType']) {
     case "Add": 
-      if (insertSaleItem($_POST ['product_id'], $_POST ['sale_id'], $_POST['quantity'],$_POST['saleprice'] )) {
+      if (insertSaleItems($_POST ['product_id'], $_POST ['sale_id'], $_POST['quantity'],$_POST['saleprice'] )) {
         echo '<div class="alert alert-success" role="alert"> SaleItem added.</div>'; 
       } else {
         echo '<div class="alert alert-danger" role="alert"> Error.</div>';
       }
       break; 
     case "Edit": 
-      if (updateSaleItem($_POST['Saleitem_id'], $_POST['product_id'], $_POST['sale_id'], $_POST['quantity'], $_POST['saleprice'])) {
+      if (updateSaleItems($_POST['Saleitem_id'], $_POST['product_id'], $_POST['sale_id'], $_POST['quantity'], $_POST['saleprice'])) {
             echo '<div class="alert alert-success" role="alert"> SalItem edited.</div>';
         } else {
             echo '<div class="alert alert-danger" role="alert"> Error.</div>';
@@ -22,7 +22,7 @@ if (isset($_POST ['actionType'])){
 
       break; 
     case "Delete":
-      if (deleteSaleItem($_POST['Saleitem_id'])) {
+      if (deleteSaleItems($_POST['Saleitem_id'])) {
         echo '<div class="alert alert-success" role="alert"> SaleItem deleted.</div>'; 
       } else {
         echo '<div class="alert alert-danger" role="alert"> Error.</div>';
