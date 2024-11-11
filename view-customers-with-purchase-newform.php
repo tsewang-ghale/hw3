0aac080e6abe2387c293439577cd6960f2ec5dab
tsewang-ghale <Tsewang.g@ou.edu>
2024-11-11 13:29:48 -0600

    Update and rename view-customers-with-purchase-newform1.php to view-customers-with-purchase-newform.php

--- view-customers-with-purchase-newform.php
+++ view-customers-with-purchase-newform.php
@@ -1,6 +1,6 @@
 
 <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newform1CustomerPurchaseModal">
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newformCustomerWithPurchaseModal">
        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
@@ -8,11 +8,11 @@
 </button>
 
       <!-- Modal -->
-      <div class="modal fade" id="newform1CustomerPurchaseModal" tabindex="-1" aria-labelledby="newform1CustomerPurchaseModalLabel" aria-hidden="true">
+      <div class="modal fade" id="newformCustomerWithPurchaseModal" tabindex="-1" aria-labelledby="newformCustomerWithPurchaseModalLabel" aria-hidden="true">
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
-              <h1 class="modal-title fs-5" id="newform1CustomerPurchaseModalLabel"> New Customers with Purchase</h1>
+              <h1 class="modal-title fs-5" id="newformCustomerWithPurchaseModalLabel"> New Customers with Purchase</h1>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
