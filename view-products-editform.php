481cbef1ac1edbe230be67aacb56069a76ee7bbb
tsewang-ghale <Tsewang.g@ou.edu>
2024-11-11 01:07:05 -0600

    Update view-products-editform.php

--- view-products-editform.php
+++ view-products-editform.php
@@ -1,9 +1,8 @@
-<!-- Button trigger modal -->
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editProductModal<?php echo $Product['product_id']; ?>">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
-    </svg>
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+    <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+  </svg>
 </button>
 
 <!-- Modal -->
@@ -11,11 +10,15 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="editProductModalLabel<?php echo $Product['product_id']; ?>">Edit Product</h1>
+        <h1 class="modal-title fs-5" id="editProductModalLabel<?php echo $Product['product_id']; ?>">Edit Sale Item</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         <form method="post" action="">
+          <div class="mb-3">
+            <label for="product_id_<?php echo $Product['product_id']; ?>" class="form-label">Product ID</label>
+            <input type="number" class="form-control" id="product_id_<?php echo $Product['product_id']; ?>" name="product_id" value="<?php echo $Product['product_id']; ?>">
+          </div>
           <div class="mb-3">
             <label for="product_name_<?php echo $Product['product_id']; ?>" class="form-label">Product Name</label>
             <input type="text" class="form-control" id="product_name_<?php echo $Product['product_id']; ?>" name="product_name" value="<?php echo $Product['product_name']; ?>">
@@ -26,18 +29,18 @@
           </div>
           <div class="mb-3">
             <label for="listprice_<?php echo $Product['product_id']; ?>" class="form-label">List Price</label>
-            <input type="number" class="form-control" id="listprice_<?php echo $Product['product_id']; ?>" name="listprice" value="<?php echo $Product['listprice']; ?>">
+            <input type="number" class="form-control" id="listprice_<?php echo $Product['product_id']; ?>" name="listprice" step="0.01" value="<?php echo $Product['listprice']; ?>">
           </div>
-          <div class="mb-3">
+        <div class="mb-3">
             <label for="color_<?php echo $Product['product_id']; ?>" class="form-label">Color</label>
             <input type="text" class="form-control" id="color_<?php echo $Product['product_id']; ?>" name="color" value="<?php echo $Product['color']; ?>">
           </div>
-          <div class="mb-3">
+            <div class="mb-3">
             <label for="category_<?php echo $Product['product_id']; ?>" class="form-label">Category</label>
             <input type="text" class="form-control" id="category_<?php echo $Product['product_id']; ?>" name="category" value="<?php echo $Product['category']; ?>">
-          </div>  
+          </div>
           <input type="hidden" name="product_id" value="<?php echo $Product['product_id']; ?>">
-          <input type="hidden" name="actionType" value="Edit">
+          <input type="hidden" name="actionType" value="Edit"> 
           <button type="submit" class="btn btn-primary">Save</button>
         </form>
       </div>
