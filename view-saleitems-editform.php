37f0d8ed4e6527bed6fb1971ca062a191a550450
tsewang-ghale <Tsewang.g@ou.edu>
2024-11-10 22:08:22 -0600

    Update view-saleitems-editform.php

--- view-saleitems-editform.php
+++ view-saleitems-editform.php
@@ -1,4 +1,3 @@
-!-- Button trigger modal -->
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editSaleItemModal<?php echo $saleitem['Saleitem_id'];?>">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
@@ -30,7 +29,7 @@
           </div>
            <div class="mb-3">
             <label for="saleprice <?php echo $saleitem['Saleitem_id']; ?>" class="form-label">Sale Price</label>
-            <input type="decimal" class="form-control" id="saleprice <?php echo $saleitem['Saleitem_id']; ?>" name = "saleprice" value = "<?php echo $saleitem['saleprice']; ?>">
+            <input type="integer" class="form-control" id="saleprice <?php echo $saleitem['Saleitem_id']; ?>" name = "saleprice" value = "<?php echo $saleitem['saleprice']; ?>">
           </div>
             <input type="hidden" name = "Saleitem_id" value = "<?php echo $saleitem['Saleitem_id']; ?>">
           <input type = "hidden" name = "actionType" value = "Edit"> 
