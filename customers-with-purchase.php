76c491c48e452aa3001de2029b3cecf65b72ba96
tsewang-ghale <Tsewang.g@ou.edu>
2024-11-11 22:05:04 -0600

    Update customers-with-purchase.php

--- customers-with-purchase.php
+++ customers-with-purchase.php
@@ -14,14 +14,14 @@ if (isset($_POST ['actionType'])){
         }
       break; 
     case "Edit": 
-      if (UpdateCustomersPurchase($_POST['sid'], $_POST['cust_id'],$_POST['cust_firstname'], $_POST['cust_lastname'], $_POST['product_name'], $_POST['tax'], $_POST['shipping'], $_POST['quantity'], $_POST['saleprice'])) {
+      if (UpdateCustomersPurchase($_POST['sale_id'], $_POST['cust_id'],$_POST['cust_firstname'], $_POST['cust_lastname'], $_POST['product_name'], $_POST['tax'], $_POST['shipping'], $_POST['quantity'], $_POST['saleprice'])) {
             echo '<div class="alert alert-success" role="alert"> Sale edited.</div>';
         } else {
             echo '<div class="alert alert-danger" role="alert"> Error.</div>';
         }
       break; 
     case "Delete":
-      if (deleteCustomersPurchase($_POST['sid'])) {
+      if (deleteCustomersPurchase($_POST['sale_id'])) {
         echo '<div class="alert alert-success" role="alert"> Sale deleted.</div>'; 
       } else {
         echo '<div class="alert alert-danger" role="alert"> Error.</div>';
