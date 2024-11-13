f45fdbd6f8d89f12e53b881a58de03e61ce008c4
tsewang-ghale <Tsewang.g@ou.edu>
2024-11-13 10:13:53 -0600

    Update model-products-chart.php

--- model-products-chart.php
+++ model-products-chart.php
@@ -1,12 +1,10 @@
 <?php
-require_once("util-db.php");
-
 function selectProducts() {
     try {
         $conn = get_db_connection();  // Get the database connection
         
-        // SQL query to fetch product sales count
-        $stmt = $conn->prepare(" SELECT product_name, COUNT(product_id) AS product_count FROM `product`  GROUP BY product_name");
+        // Prepare the SQL query to fetch product sales count
+        $stmt = $conn->prepare("SELECT product_name, COUNT(product_id) AS product_count FROM `product` GROUP BY product_name");
         
         $stmt->execute();  // Execute the query
         $result = $stmt->get_result();  // Get the result
