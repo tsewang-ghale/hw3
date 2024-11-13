fe202f286657ca9f79ac8183a46c23942ef5ec93
tsewang-ghale <Tsewang.g@ou.edu>
2024-11-12 22:09:11 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -61,15 +61,6 @@
             <li><a href="get.php">Go to Get Page</a></li>
         </ul>
     </div>
-
-    <!-- Footer -->
-    <footer class="bg-light text-center py-3 mt-5">
-        <p>&copy; <?= date("Y"); ?> My Website | All rights reserved.</p>
-    </footer>
-
-    <!-- Bootstrap JS (Including Popper.js) -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
