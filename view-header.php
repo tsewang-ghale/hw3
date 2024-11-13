ac080d441eaa992185f7651f197e09e9b1924f48
tsewang-ghale <Tsewang.g@ou.edu>
2024-11-12 22:17:21 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -1,54 +1,43 @@
 <!doctype html>
 <html lang="en">
-
-<head>
+  <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= htmlspecialchars($pageTitle); ?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-
-<body>
-
-    <!-- Navbar -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php">Homework 6</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="customers.php">Customers</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="sales.php">Sales</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="customers-with-purchase.php">Customers With Purchases</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="saleitems.php">Sale Items</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="products.php">Products</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="customers-chart.php">Customers Chart</a>
-                    </li>
-                </ul>
-            </div>
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="customers.php">Customers</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="sales.php"> Sales</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="customers-with-purchase.php"> Customers With Purchases</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="saleitems.php"> Saleitems </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="products.php"> Products </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="customers-chart.php"> Products </a>
+            </li>
+            
+          </ul>
         </div>
+      </div>
     </nav>
-
-    <!-- Page Title -->
-    <header class="bg-primary text-white text-center py-5">
-        <h1 class="display-4"><?= htmlspecialchars($pageTitle); ?></h1>
-    </header>
-
