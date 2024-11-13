18d24e9b593bd64a0f93f80cb2ed82b42bb6516a
tsewang-ghale <Tsewang.g@ou.edu>
2024-11-12 22:12:00 -0600

    Update view-footer.php

--- view-footer.php
+++ view-footer.php
@@ -1,11 +1,11 @@
-<footer class="bg-light text-center py-3 mt-5">
+    <!-- Footer -->
+    <footer class="bg-light text-center py-3 mt-5">
         <p>&copy; <?= date("Y"); ?> My Website | All rights reserved.</p>
     </footer>
 
     <!-- Bootstrap JS (Including Popper.js) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
 </body>
 </html>
-
- 
