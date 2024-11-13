8e92a66a76b325f9efbd338aa70d9844c9e6d2c6
tsewang-ghale <Tsewang.g@ou.edu>
2024-11-12 22:11:36 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -52,15 +52,3 @@
         <h1 class="display-4"><?= htmlspecialchars($pageTitle); ?></h1>
     </header>
 
-    <!-- Main Content -->
-    <div class="container mt-5">
-        <h2 class="mb-4">Welcome to the Dashboard</h2>
-        <p>This is where you can explore data and insights. Below are some links to other pages:</p>
-        <ul>
-            <li><a href="post.php">Go to Post Page</a></li>
-            <li><a href="get.php">Go to Get Page</a></li>
-        </ul>
-    </div>
-</body>
-
-</html>
