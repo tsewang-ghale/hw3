57bb1fd1a27ab4428529f87adfb165684f179673
tsewang-ghale <Tsewang.g@ou.edu>
2024-11-13 10:15:37 -0600

    Update products-chart.php

--- products-chart.php
+++ products-chart.php
@@ -2,7 +2,7 @@
 require_once("util-db.php");
 require_once("model-products-chart.php");
 
-$pageTitle = "Product Sales Chart";  // Set the page title
+$pageTitle = "Products Chart";  // Set the page title
 include "view-header.php";  // Include the header
 $products = selectProducts();  // Fetch the products data
 include "view-products-chart.php";  // Display the chart view
