c160847e5d2b0d6389a08dae91ad3046bab5d4a1
tsewang-ghale <Tsewang.g@ou.edu>
2024-11-13 09:50:35 -0600

    Update model-saleitems-chart.php

--- model-saleitems-chart.php
+++ model-saleitems-chart.php
@@ -3,7 +3,7 @@ function selectSaleItems() {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("
-        SELECT si.saleitem_id,si.product_id,(si.quantity * si.saleprice) AS total_price FROM `SaleItem` group by si.saleitem_id,si.product_id ");
+        SELECT si.saleitem_id,si.product_id,(si.quantity * si.saleprice) AS total_price FROM `SaleItem` si group by si.saleitem_id,si.product_id ");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
