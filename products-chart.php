a21e926117ffbbae46b58857306cf0fd3a60ef79
tsewang-ghale <Tsewang.g@ou.edu>
2024-11-13 09:55:27 -0600

    Create products-chart.php

---
+++ products-chart.php
@@ -0,0 +1,11 @@
+<?php
+// Include the necessary files
+require_once("util-db.php");
+require_once("model-products-chart.php");
+
+$pageTitle = "Product Sales Chart";  // Set the page title
+include "view-header.php";  // Include the header
+$products = selectProducts();  // Fetch the products data
+include "view-products-chart.php";  // Display the chart view
+include "view-footer.php";  // Include the footer
+?>
