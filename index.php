0ea0d6c76d2cecd1844421c23a52d3ed736bf11a
tsewang-ghale <Tsewang.g@ou.edu>
2024-11-12 22:16:32 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
-<?php $pageTitle = "Dashboard"; ?>
-
-<?php include('header.php'); ?>
-
-<!-- Main Content -->
-<div class="container mt-5">
-    <h2 class="mb-4">Welcome to the Dashboard</h2>
-    <p>This is where you can explore data and insights. Below are some links to other pages:</p>
+<?php
+$pageTitle = "Home";
+include "view-header.php";
+?>
+    <h1> Homework 3</h1>
     <ul>
         <li><a href="post.php">Go to Post Page</a></li>
         <li><a href="get.php">Go to Get Page</a></li>
     </ul>
-</div>
-
-<?php include('footer.php'); ?>
+<?php
+include "view-footer.php";
+?>
