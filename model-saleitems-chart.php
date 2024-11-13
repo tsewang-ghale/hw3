f3a00fd2491c5a2f06618a0dbaf6679e7bf5cce8
tsewang-ghale <Tsewang.g@ou.edu>
2024-11-12 23:22:56 -0600

    Update model-saleitems-chart.php

--- model-saleitems-chart.php
+++ model-saleitems-chart.php
@@ -4,9 +4,9 @@ function selectSaleItems() {
         $conn = get_db_connection();  // Get the database connection
         
         // Prepare the query to select sale items data
-        $stmt = $conn->prepare("SELECT product_id, sale_id, quantity, saleprice, COUNT(Saleitem_id) AS count_saleitem 
+        $stmt = $conn->prepare("SELECT sale_id, quantity, saleprice, COUNT(Saleitem_id) AS count_saleitem 
                                 FROM saleitem si join sale s on si.saleid= s.saleid
-                                GROUP BY product_id, sale_id, quantity, saleprice");
+                                GROUP BY sale_id, quantity, saleprice");
         $stmt->execute();  // Execute the query
         $result = $stmt->get_result();  // Get the result
         $conn->close();  // Close the connection
