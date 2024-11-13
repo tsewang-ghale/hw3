7a96995cb21621b97642d7ee6ad9ca1529c03773
tsewang-ghale <Tsewang.g@ou.edu>
2024-11-13 09:30:36 -0600

    Update saleitems-chart.php

--- saleitems-chart.php
+++ saleitems-chart.php
@@ -4,7 +4,7 @@ require_once("model-saleitems-chart.php");
   
 $pageTitle = "Sale Items Chart";  // Title for the page
 include "view-header.php";  // Include the header view
-$saleitems = selectSaleItems();  // Fetch the sale items data using the selectSaleItems function
+$saleitems = selectSaleItems()  // Fetch the sale items data using the selectSaleItems function
 include "view-saleitems-chart.php";  // Include the chart view
 include "view-footer.php";  // Include the footer view
 ?>
