081f43fc5cab1fc121a0d47f706efe45eba23b18
tsewang-ghale <Tsewang.g@ou.edu>
2024-11-12 21:38:18 -0600

    Update customers-chart.php

--- customers-chart.php
+++ customers-chart.php
@@ -1,6 +1,6 @@
 <?php
 require_once("util-db.php");
-require_once("model-customers-chart-db.php");
+require_once("model-customers-chart.php");
   
 $pageTitle = "Customers Chart";
 include "view-header.php";
