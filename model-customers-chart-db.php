dd6ad948655ea2334545db2200a288792574f628
tsewang-ghale <Tsewang.g@ou.edu>
2024-11-12 20:44:31 -0600

    Update model-customers-chart-db.php

--- model-customers-chart-db.php
+++ model-customers-chart-db.php
@@ -2,7 +2,7 @@
 function selectCustomers() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT cust_firstname, cust_lastname, count(s.Sale_id) as numberofitemsbought FROM `Customer` c join `Sale` s on c.cust_id= s.cust_id group by cust_firstname, cust_lastname");
+        $stmt = $conn->prepare("SELECT cust_lastname, count(s.Sale_id) as count_sale FROM `Customer` c join `Sale` s on c.cust_id= s.cust_id group by cust_firstname, cust_lastname");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
