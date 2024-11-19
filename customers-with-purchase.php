72eb6c080fc3546c42fb99fb51e46b7e3776ba48
tsewang-ghale <Tsewang.g@ou.edu>
2024-11-19 15:21:24 -0600

    Update customers-with-purchase.php

--- customers-with-purchase.php
+++ customers-with-purchase.php
@@ -4,32 +4,6 @@ require_once("model-customers-with-purchase.php");
   
 $pageTitle = "Customers with Purchase";
 include "view-header.php";
-if (isset($_POST ['actionType'])){
-  switch ($_POST ['actionType']) {
-    case "Add": 
-     if (insertCustomersPurchase($_POST['cust_id'], $_POST['cust_firstname'], $_POST['cust_lastname'], $_POST['product_name'], $_POST['sale_date'], $_POST['tax'], $_POST['shipping'], $_POST['quantity'], $_POST['saleprice'])) {
-            echo '<div class="alert alert-success" role="alert"> Sale added.</div>'; 
-        } else {
-            echo '<div class="alert alert-danger" role="alert"> Error.</div>';
-        }
-      break; 
-    case "Edit": 
-      if (UpdateCustomersPurchase($_POST['sale_id'], $_POST['cust_id'],$_POST['cust_firstname'], $_POST['cust_lastname'], $_POST['product_name'], $_POST['tax'], $_POST['shipping'], $_POST['quantity'], $_POST['saleprice'])) {
-            echo '<div class="alert alert-success" role="alert"> Sale edited.</div>';
-        } else {
-            echo '<div class="alert alert-danger" role="alert"> Error.</div>';
-        }
-      break; 
-    case "Delete":
-      if (deleteCustomersPurchase($_POST['sale_id'])) {
-        echo '<div class="alert alert-success" role="alert"> Sale deleted.</div>'; 
-      } else {
-        echo '<div class="alert alert-danger" role="alert"> Error.</div>';
-      }
-      break;
-  }
-}
-
 $customers = selectCustomers();
 include "view-customers-with-purchase.php";
 include "view-footer.php";
