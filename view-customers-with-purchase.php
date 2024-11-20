4ce5ca84b51111ccbe8c193ab750ad5c2034e203
tsewang-ghale <Tsewang.g@ou.edu>
2024-11-20 00:06:44 -0600

    Update view-customers-with-purchase.php

--- view-customers-with-purchase.php
+++ view-customers-with-purchase.php
@@ -43,7 +43,7 @@ while ($customer = $customers->fetch_assoc()) {
       ?> 
       
       <form method= "post" action= "">
-        <input type= "hidden" name = "Cust_id" value= "<?php echo $customer['cust_id']; ?>">
+        <input type= "hidden" name = "cust_id" value= "<?php echo $customer['cust_id']; ?>">
         <input type= "hidden" name= "actionType" value = "Delete">
         <button type="submit" class="btn btn-primary" onclick= "return confirm ('Are you sure?');">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
