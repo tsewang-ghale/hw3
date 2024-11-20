b083d0d42f15000cbc84bae08f47e9e7d0cd5a2b
tsewang-ghale <Tsewang.g@ou.edu>
2024-11-19 19:19:59 -0600

    Update view-customers-with-purchase.php

--- view-customers-with-purchase.php
+++ view-customers-with-purchase.php
@@ -1,44 +1,24 @@
-<h1> Customers with Purchase </h1>
-<div class="card-group">
+<h1> Customers with Purchase</h1>
+ <div class="card-group">
 <?php
-while ($customer = $customers->fetch_assoc()) {
+while ($customer= $customers -> fetch_assoc()){
 ?>
   <div class="card">
     <div class="card-body">
-      <h5 class="card-title"><?php echo $customer['cust_firstname']; ?> <?php echo $customer['cust_lastname']; ?></h5>
+      <h5 class="card-title"><?php echo $customer['cust_firstname'] ?>;<?php echo $customer['cust_lastname']; ?> </h5>
       <p class="card-text">
-        <ul class="list-group">
-        <?php
-          $sales = selectCustomersPurchase($customer['cust_id']);
-          while ($sale = $sales->fetch_assoc()) {
-        ?>
-          <li class="list-group-item">
-            <strong>Product:</strong> <?php echo $sale['product_name']; ?> | 
-            <strong>Date:</strong> <?php echo $sale['sale_date']; ?> | 
-            <strong>Price:</strong> <?php echo $sale['saleprice']; ?>
-          </li>
-        <?php
-          }
-        ?>
-        </ul>
+     <ul class="list-group">
+  <?php
+   $sales = selectCustomersPurchase($customer['cust_id']); 
+  while ($sale= $sales -> fetch_assoc()){
+  ?> 
+        <li class="list-group-item"><?php echo($sale['cust_firstname']); ?>- <?php echo($sale['product_name']); ?>- <?php echo($sale['sale_date']); ?>- <?php echo($sale['saleprice']); ?></li>
+  <?php 
+  }
+  ?>
+      </ul>
       </p>
-      <p class="card-text">
-        <small class="text-body-secondary">
-          <strong>Phone:</strong> <?php echo $customer['cust_phone']; ?> | 
-          <strong>Email:</strong> <?php echo $customer['cust_email']; ?>
-        </small>
-      </p>
-      <form method="post" action="">
-        <input type="hidden" name="cust_id" value="<?php echo $customer['cust_id']; ?>">
-        <input type="hidden" name="actionType" value="Delete">
-        <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this customer?');">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
-          </svg>
-          Delete Customer
-        </button>
-      </form>
+      <p class="card-text"><small class="text-body-secondary"> Phonenumber: <?php echo $customer['cust_phone']; ?>; Email:<?php echo $customer['cust_email']; ?> </small></p>
     </div>
   </div>
 <?php
