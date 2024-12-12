8f97f4dfd19a59e34de9d1c3510cbffd61188e57
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-12 17:14:18 -0600

    Update sales.php

--- sales.php
+++ sales.php
@@ -19,20 +19,21 @@ if (isset($_POST['actionType'])) {
       
     case "Edit": 
       // Edit existing sale
-      if (isset($_POST['sid']) && isset($_POST['Cust_id']) && isset($_POST['Sale_date']) && isset($_POST['Tax']) && isset($_POST['Shipping'])) {
-        $sale_id = $_POST['sid'];
-        $cust_id = $_POST['Cust_id']; // Pass cust_id to the UpdateSale function
-        $sale_date = $_POST['Sale_date'];
-        $tax = $_POST['Tax'];
-        $shipping = $_POST['Shipping'];
+     if (isset($_POST['sid']) && isset($_POST['Cust_id']) && isset($_POST['Sale_date']) && isset($_POST['Tax']) && isset($_POST['Shipping'])) {
+    $sale_id = $_POST['sid'];
+    $cust_id = $_POST['Cust_id']; // Pass cust_id to the UpdateSale function
+    $sale_date = $_POST['Sale_date'];
+    $tax = $_POST['Tax'];
+    $shipping = $_POST['Shipping'];
 
-        // Call the UpdateSale function with all parameters
-        if (UpdateSale($sale_id, $cust_id, $sale_date, $tax, $shipping)) {
-            echo '<div class="alert alert-success" role="alert"> Sale edited.</div>';
-        } else {
-            echo '<div class="alert alert-danger" role="alert"> Error.</div>';
-        }
-      }
+    // Call the UpdateSale function with all parameters
+    if (UpdateSale($sale_id, $cust_id, $sale_date, $tax, $shipping)) {
+        echo '<div class="alert alert-success" role="alert"> Sale edited.</div>';
+    } else {
+        echo '<div class="alert alert-danger" role="alert"> Error.</div>';
+    }
+}
+
       break; 
 
     case "Delete":
