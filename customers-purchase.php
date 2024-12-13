9347bcd62199b21914c65162fb7f704996bdec10
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-13 05:10:56 -0600

    Update customers-purchase.php

--- customers-purchase.php
+++ customers-purchase.php
@@ -4,8 +4,15 @@ require_once("model-customers-with-purchase.php");
 
 $pageTitle = "Customers Purchases";
 include "view-header.php";
-$customers = selectCustomersPurchase($_GET['id']);
+
+// Check if 'id' is set and is a valid number
+if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+    $customers = selectCustomersPurchase($_GET['id']);
+} else {
+    echo "Invalid or missing 'id' parameter.";
+    exit;
+}
+
 include "view-customers-purchase.php";
 include "view-footer.php";
 ?>
-
